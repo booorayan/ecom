c56990d631d24206d38aa627a4f1817609195f08
brian <bryantlch@gmail.com>
2023-04-25 13:13:18 +0300

    order summary

--- core/models.py
+++ core/models.py
@@ -53,6 +53,20 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.quantity} of {self.item.item_name}"
 
+    def get_total_item_price(self):
+        return self.quantity * self.item.price
+
+    def get_discount_item_price(self):
+        return self.quantity * self.item.discount_price
+
+    def get_amount_saved(self):
+        return self.get_total_item_price() - self.get_discount_item_price()
+
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_discount_item_price()
+        return self.get_total_item_price()
+
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -64,5 +78,13 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_total_price(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
+
+
 
 
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import remove_from_cart, add_to_cart, ProductView, HomeView
+from .views import remove_from_cart, reduce_quantity_item, add_to_cart, ProductView, HomeView, OrderSummaryView
 
 app_name = 'core'
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<pk>/', ProductView.as_view(), name='product'),
     path('add-to-cart/<pk>/', add_to_cart, name='add-to-cart'),
-    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart')
+    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart'),
+    path('order-summary', OrderSummaryView.as_view(), name='order-summary'),
+    path('reduce-quantity-item/<pk>/', reduce_quantity_item, name='reduce-quantity-item')
 ]
