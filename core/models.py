347dc829a6729d5a12fcadcb412ed236a1a2ae3f
brian <bryantlch@gmail.com>
2023-04-27 15:37:32 +0300

    add payment model

--- core/models.py
+++ core/models.py
@@ -102,5 +102,15 @@ class CheckoutAddress(models.Model):
         return self.user.username
 
 
+class Payment(models.Model):
+    stripe_id = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.SET_NULL, blank=True, null=True)
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
+
 
 
